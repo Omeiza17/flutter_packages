58177a59deccf4a284538a9d33cc36a0f2c43ddd
codingstoic <mosuleiman19@gmail.com>
2021-03-05 22:35:23 +0400

    Implemented third party authentication using google_sign_in.

--- packages/firebase_auth_service/test/firebase_auth_service_test.dart
+++ packages/firebase_auth_service/test/firebase_auth_service_test.dart
@@ -1,9 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firebase_auth_service/firebase_auth_service.dart';
-
 void main() {
-  test('adds one to input values', () {
-
-  });
+  test('adds one to input values', () {});
 }
--- packages/third_party_auth/lib/third_party_auth.dart
+++ packages/third_party_auth/lib/third_party_auth.dart
@@ -1,7 +1,37 @@
 library third_party_auth;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_service/firebase_auth_service.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class ThirdPartyAuth {
+  Future<AppUser> signInWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await FirebaseAuth.instance
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          // Note: Access token is null when running on web, so we don't check for it above
+          accessToken: googleAuth.accessToken,
+        ));
+        return AppUser.fromFirebaseUser(userCredential.user);
+      } else {
+        throw FirebaseException(
+          plugin: runtimeType.toString(),
+          code: 'ERROR_MISSING_GOOGLE_ID_TOKEN',
+          message: 'Missing Google ID Token',
+        );
+      }
+    } else {
+      throw FirebaseException(
+        plugin: runtimeType.toString(),
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
 }
